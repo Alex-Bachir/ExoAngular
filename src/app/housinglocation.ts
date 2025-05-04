c1422983bc8254ae8217d20ed09aecc37b7e14f2
alex-bachir <alex.bachir@laplateforme.io>
2025-05-04 18:30:11 +0200

    rajout conseil interface imbriquée

--- src/app/housinglocation.ts
+++ src/app/housinglocation.ts
@@ -7,4 +7,31 @@ export interface HousingLocation {
     availableUnits: number;
     wifi: boolean;
     laundry: boolean;
-  }
+}
+
+
+//Exemple d'interface imbriquée
+
+export interface Figurine {
+  nom:string, 
+  taille: Taille;
+  licence: Licence;
+}
+
+export interface Taille {
+  hauteur: number;
+}
+
+export interface Licence {
+  nom: string;
+}
+
+const mafigurine: Figurine = {
+    nom: "",
+    taille: {
+      hauteur: 12
+    },
+    licence: {
+      nom: "marvel"
+    }
+}
